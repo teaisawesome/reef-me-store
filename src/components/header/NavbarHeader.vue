a6b19c63c9049f667f16fc1e721ca88fb39e0bd7
teaisawesome <dekany.r123@gmail.com>
2024-02-22 00:30:33 +0100

    feat(navbar): main logo packed to a router link to landing page

--- src/components/header/NavbarHeader.vue
+++ src/components/header/NavbarHeader.vue
@@ -1,8 +1,10 @@
 <template>
     <nav class="h-10vh flex justify-between text-main-txt-color">
         <div class="text-1/5xl ml-16 flex justify-center items-center">
-            <span>Reef Me</span>
-            <span>🪸</span>
+            <router-link to="/">
+                <span>Reef Me</span>
+                <span>🪸</span>
+            </router-link>
         </div>
         <div class="mr-16 flex justify-around w-1/5">
             <router-link class="flex justify-center items-center" to="/">Home</router-link>
